9aa1472e7a3db5ab53aea8675b953f95df53bddf
a1q123456 <a1q123456@gmail.com>
2023-02-09 19:46:03 +0000

    Updated the style

--- src/components/categories-header.tsx
+++ src/components/categories-header.tsx
@@ -2,22 +2,30 @@ import PostDataContext from "@/contexts/post-data-context"
 import { Category } from "@/models/category"
 import Link from 'next/link'
 import styles from "@/styles/category-header.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const CategoriesHeader = () => {
 
     const renderPostHeader = (categories: Category[]) => {
         return <div className={styles.header}>
+            <input type="checkbox" hidden className={styles.headerExpandToggle} id="header-expand-toggle" />
             <p className={styles.headerTitle}>{"TechBlog();"}</p>
+            <label htmlFor="header-expand-toggle" className={styles.headerItemButton}>
+                <FontAwesomeIcon icon={faBars} />
+            </label>
             <div className={styles.headerItemList}>
-                <Link className={`${styles.headerItem}`} href='/'><span>Home</span></Link>
-                {
-                    categories.map(category => <Link className={`${styles.headerItem}`} key={category.id} href={{
-                        pathname: '/category/[categoryId]',
-                        query: { categoryId: category.id }
-                    }}><span>{category.title}</span></Link>)
-                }
+                <div className={styles.headerItemWrapper}>
+                    <Link className={`${styles.headerItem}`} href='/'><span>Home</span></Link>
+                    {
+                        categories.map(category => <Link className={`${styles.headerItem}`} key={category.id} href={{
+                            pathname: '/category/[categoryId]',
+                            query: { categoryId: category.id }
+                        }}><span>{category.title}</span></Link>)
+                    }
+                </div>
             </div>
-        </div>
+        </div >
     }
 
     return <PostDataContext.Consumer>
